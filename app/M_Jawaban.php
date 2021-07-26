1f4dea69a5e843abd413e8c85b83a03d8086035d
oktyajipangestu <oktyajipangestu@gmail.com>
2021-07-26 16:40:55 +0700

    pembuatan model soal, jawaban, skor

---
+++ app/M_Jawaban.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class M_Jawaban extends Model
+{
+    protected $table = 'tbl_jawaban';
+    protected $primaryKey = 'id_jawaban';
+    protected $fillable = [
+        'id_jawaban',
+        'id_skor',
+        'id_peserta',
+        'id_soal',
+        'jawaban',
+        'status_jawaban'
+    ];
+}
